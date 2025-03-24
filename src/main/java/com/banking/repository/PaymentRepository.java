25ea387ed52e72b196bd30e783329783a3cb440e
Chekuri-Mohit <mc185480@ncr.com>
2025-03-24 12:49:34 +0530

    git commit -m "Add payment history"

--- src/main/java/com/banking/repository/PaymentRepository.java
+++ src/main/java/com/banking/repository/PaymentRepository.java
@@ -6,9 +6,11 @@ import com.banking.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     @Query("""
